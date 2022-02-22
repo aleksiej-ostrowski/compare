0c74f101746e990ec295216054ce9868b5033310
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2022-02-22 23:27:04 +0300

    go get Profiling

--- INSTALL/install_modules.sh
+++ INSTALL/install_modules.sh
@@ -4,8 +4,10 @@
 
 # go get -u github.com/wk8/go-ordered-map
 
-go get -u github.com/shirou/gopsutil/cpu
-go get -u github.com/shirou/gopsutil/mem   
+# go get -u github.com/shirou/gopsutil/cpu
+# go get -u github.com/shirou/gopsutil/mem   
+
+go get -u github.com/pkg/profile
 
 # /usr/local/go/bin/go get -u github.com/jinzhu/copier
 # /usr/local/go/bin/go get -u github.com/mattn/go-sqlite3
